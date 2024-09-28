f2881cdca46c95e61e09b9f0a7825b84e1a3e944
Licates <136702730+Licates@users.noreply.github.com>
2024-09-28 17:01:50 +0200

    Add bilstm_classification but still classifying all same

--- enfify/__init__.py
+++ enfify/__init__.py
@@ -1,4 +1,4 @@
-from enfify.classification import *  # noqa: F401, F403
+from enfify.cnn_classification import *  # noqa: F401, F403
 from enfify.config import *  # noqa: F401, F403
 from enfify.feature_calculation import *  # noqa: F401, F403
 from enfify.pipeline import *  # noqa: F401, F403
